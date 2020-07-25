a7e5278ceb5223a073147d9466e7251deec55237
liangqi1 <liangqi000@gmail.com>
2020-07-25 10:27:50 +0800

    finish desc cmd

--- app/output/table.go
+++ app/output/table.go
@@ -5,7 +5,37 @@ import (
 	"os"
 )
 
-func Table(tbData [][]string)  {
+func DescTable(tbData [][]string) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"--", "Desc"})
+	hColor := tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiCyanColor}
+	table.SetHeaderColor(hColor, hColor)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	for index, row := range tbData {
+		if index == 0 {
+			switch row[1] {
+			case "running":
+				table.Rich(row, []tablewriter.Colors{{}, {tablewriter.Bold, tablewriter.FgHiGreenColor}})
+				break
+			case "stopped":
+				table.Rich(row, []tablewriter.Colors{{}, {tablewriter.Bold, tablewriter.FgHiYellowColor}})
+				break
+			case "failed":
+				table.Rich(row, []tablewriter.Colors{{}, {tablewriter.Bold, tablewriter.FgRedColor}})
+			default:
+				table.Append(row)
+			}
+		} else {
+			table.Append(row)
+		}
+	}
+
+	//table.SetRowLine(true)
+	table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_LEFT})
+	table.Render()
+}
+
+func Table(tbData [][]string) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Id", "Name", "Pid", "Status", "Date"})
 	hColor := tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiCyanColor}
