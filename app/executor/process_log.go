72726c1edc5c68152498a6d461f1d2881265765f
liangqi1 <liangqi000@gmail.com>
2020-07-29 23:29:31 +0800

    finish stop command

---
+++ app/executor/process_log.go
@@ -0,0 +1,34 @@
+package executor
+
+import (
+	"github.com/ntt360/pmon2/app"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func getLogPath(customLogFile string, hash string) (string, error) {
+	prjDir := strings.TrimRight(app.Config.GetLogsDir(), "/")
+	if len(customLogFile) <= 0 {
+		_, err := os.Stat(prjDir)
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(prjDir, 0755)
+			if err != nil {
+				return "", err
+			}
+		}
+		customLogFile = prjDir + "/" + hash
+	}
+
+	return customLogFile, nil
+}
+
+func getLogFile(customLogFile string) (*os.File, error) {
+	// 创建进程日志文件
+	logFile, err := os.OpenFile(customLogFile, syscall.O_CREAT|syscall.O_APPEND|syscall.O_WRONLY, 0755)
+	if err != nil {
+		return nil, err
+	}
+
+	return logFile, nil
+}
--- app/init.go
+++ app/init.go
@@ -22,7 +22,7 @@ func init() {
 	Log.SetFormatter(&logrus.TextFormatter{
 		DisableTimestamp: true,
 	})
-	//Log.SetReportCaller(true)
+	Log.SetReportCaller(true)
 }
 
 func Instance(confDir string) {
