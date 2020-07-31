540e75f1fe19078111f44727afe6335563be9357
liangqi1 <liangqi000@gmail.com>
2020-07-31 19:12:34 +0800

    monitor code

--- app/god/monitor.go
+++ app/god/monitor.go
@@ -2,7 +2,9 @@ package god
 
 import (
 	"fmt"
+	"github.com/ntt360/pmon2/app"
 	"github.com/ntt360/pmon2/app/model"
+	"os"
 )
 
 var MonQueue chan *model.Process
@@ -17,10 +19,35 @@ func NewMonitor() {
 }
 
 func runMonitor() {
-	for {
-		select {
-		case process := <-MonQueue:
-			fmt.Println(process.MustJson())
-		}
+	var all []model.Process
+	err := app.Db().Find(&all).Error
+	if err != nil {
+		return
 	}
+
+	for _, process := range all {
+		if process.Status == model.StatusStopped || process.Status == model.StatusInit{
+			continue
+		}
+
+		// just check failed process
+		go restartProcess(process)
+	}
+}
+
+func restartProcess(p model.Process) {
+	_, err := os.Stat(fmt.Sprintf("/proc/%d/status", p.Pid))
+	if err == nil {
+		return
+	}
+
+	// proc status file not exit
+	if os.IsNotExist(err) && (p.Status == model.StatusRunning || p.Status == model.StatusFailed) {
+		execStartProc(p)
+	}
+}
+
+func execStartProc(p model.Process) {
+	//args = append([]string{"restart", p.ProcessFile}, args[1:]...)
+	//data, err := proxy.RunProcess(args)
 }
