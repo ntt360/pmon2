586764ef4e19948f881d3306429af85c30fa14b9
liangqi1 <liangqi000@gmail.com>
2020-08-13 10:58:27 +0800

    remove debug config file

--- pmon2.go
+++ pmon2.go
@@ -7,8 +7,7 @@ import (
 )
 
 func main() {
-	//conf := "/etc/pmon2/config/config.yml"
-	conf := "/home/liangqi1/devspace/pmon2/config/config.dev.yml"
+	conf := "/etc/pmon2/config/config.yml"
 	err := app.Instance(conf)
 	if err != nil {
 		log.Fatal(err)
